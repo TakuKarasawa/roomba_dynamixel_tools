6c45cd364dbf04718d6a69fe54fbd077ae2236a4
TakuKarasawa <taku-ka@outlook.com>
2021-06-11 14:43:59 +0900

    update

--- roomba_dynamixel_controller/src/roomba_dynamixel_controller.cpp
+++ roomba_dynamixel_controller/src/roomba_dynamixel_controller.cpp
@@ -12,7 +12,7 @@ RoombaDynamixelController::RoombaDynamixelController() : private_nh("~")
     private_nh.param("dynamixel_z",dynamixel_z,{0.663});
     private_nh.param("dynamixel_roll",dynamixel_roll,{0.0});
     private_nh.param("dynamixel_pitch",dynamixel_pitch,{0.0});
-    
+
     joint_pub = nh.advertise<trajectory_msgs::JointTrajectory>("/dynamixel_workbench/joint_trajectory",1);
     angle_sub = nh.subscribe("/angle",10,&RoombaDynamixelController::angle_callback,this);
     joint_sub = nh.subscribe("/dynamixel_workbench/joint_states",10,&RoombaDynamixelController::jointstate_callback,this);
@@ -24,7 +24,7 @@ RoombaDynamixelController::RoombaDynamixelController() : private_nh("~")
 }
 
 void RoombaDynamixelController::angle_callback(const dynamixel_angle_msgs::DynamixelAngle::ConstPtr& msg)
-{   
+{
     ROS_INFO("has received angle!");
     target_angle = msg->theta;
     set_parameter(target_angle);
@@ -40,7 +40,7 @@ void RoombaDynamixelController::jointstate_callback(const sensor_msgs::JointStat
     dynamixel_pose.transform.translation.x = dynamixel_x;
     dynamixel_pose.transform.translation.y = dynamixel_y;
     dynamixel_pose.transform.translation.z = dynamixel_z;
-    
+
     tf2::Quaternion q;
     q.setRPY(dynamixel_roll,dynamixel_pitch,msg->position[0]);
     dynamixel_pose.transform.rotation.x = q.x();
